4cfda61500e966fee203e86d9324d084db960004
Rhitik_HBR <ritikbhandari17@gmail.com>
2024-06-04 17:48:29 +0545

    feat:add async await axios call

--- API/src/App.jsx
+++ API/src/App.jsx
@@ -2,18 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import PersonList from './components/PersonList'
-import PersonAdd from './components/PersonAdd'
-import PersonRemove from './components/PersonRemove'
+// import PersonList from '.././components/PersonList'
+// import PersonAdd from './components/PersonAdd'
+// import PersonRemove from './components/PersonRemove'
+import TryAA from './components/axios_with_async_await/Try'
 
 function App() {
  
 
   return (
     <>
-      <PersonAdd/>
+      {/* <PersonAdd/>
       <PersonList/>
-      <PersonRemove/>
+      <PersonRemove/> */}
+
+      <TryAA/>
+
+
     </>
   )
 }
