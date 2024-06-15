58b420b6119a1f8f812336e54d28412edbf9083d
Rhitik_HBR <ritikbhandari17@gmail.com>
2024-06-15 21:25:56 +0545

    mf

--- TodoApp/src/components/AddTodoForm.jsx
+++ TodoApp/src/components/AddTodoForm.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const AddTodoForm = ({ onAddTodo }) => {
 
     const [todo, setTodo] = useState(' ');
+
+    useEffect(()=>{
+        setTimeout(()=> console.log("hello"));
+    })
 
     const handleSubmit = (e) => {
         e.preventDefault();
